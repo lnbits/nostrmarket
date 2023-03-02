5bc6e3530897ccd2a9362028736f22200fee3294
Vlad Stan <stan.v.vlad@gmail.com>
2023-03-02 13:31:42 +0200

    feat: add basic stall details

--- static/components/stall-details/stall-details.js
+++ static/components/stall-details/stall-details.js
@@ -1,15 +1,50 @@
 async function stallDetails(path) {
   const template = await loadTemplateAsync(path)
+
   Vue.component('stall-details', {
     name: 'stall-details',
     template,
 
-    //props: ['stall-id', 'adminkey', 'inkey', 'wallet-options'],
+    props: [
+      'stall-id',
+      'adminkey',
+      'inkey',
+      'wallet-options',
+      'zone-options',
+      'currencies'
+    ],
     data: function () {
       return {
         tab: 'info',
-        relay: null
+        stall: null
+        // currencies: [],
       }
+    },
+    computed: {
+      filteredZoneOptions: function () {
+        if (!this.stall) return []
+        return this.zoneOptions.filter(z => z.currency === this.stall.currency)
+      }
+    },
+    methods: {
+      getStall: async function () {
+        try {
+          const {data} = await LNbits.api.request(
+            'GET',
+            '/nostrmarket/api/v1/stall/' + this.stallId,
+            this.inkey
+          )
+          this.stall = data
+          console.log('### this.stall', this.stall)
+        } catch (error) {
+          LNbits.utils.notifyApiError(error)
+        }
+      }
+
+    },
+    created: async function () {
+      await this.getStall()
+      console.log('### this.zoneOptions', this.zoneOptions)
     }
   })
 }
