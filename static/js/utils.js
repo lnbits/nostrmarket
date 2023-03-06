abd169e7930cb1e5cdf230d0bb3e7c95594b6672
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-03-06 09:59:17 +0000

    hash string

--- static/js/utils.js
+++ static/js/utils.js
@@ -16,3 +16,13 @@ function loadTemplateAsync(path) {
 
   return result
 }
+
+async function hash(string) {
+  const utf8 = new TextEncoder().encode(string)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', utf8)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hashHex = hashArray
+    .map(bytes => bytes.toString(16).padStart(2, '0'))
+    .join('')
+  return hashHex
+}
