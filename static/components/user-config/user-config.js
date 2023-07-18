e9a31ddc0c693938851aceb6a2ce99c12f63e70a
Vlad Stan <stan.v.vlad@gmail.com>
2023-07-18 17:08:17 +0300

    feat: user logout and data clear

--- static/components/user-config/user-config.js
+++ static/components/user-config/user-config.js
@@ -2,15 +2,27 @@ async function userConfig(path) {
     const template = await loadTemplateAsync(path)
     Vue.component('user-config', {
         name: 'user-config',
-        props: ['user',],
+        props: ['account'],
         template,
 
         data: function () {
             return {
+                clearAllData: false
             }
         },
         methods: {
-            
+            logout: async function () {
+                LNbits.utils
+                    .confirmDialog(
+                        'Please make sure you save your private key! You will not be able to recover it later!'
+                    )
+                    .onOk(async () => {
+                        this.$emit('logout', { clearAllData: this.clearAllData })
+                    })
+            },
+            copyText(text) {
+                this.$emit('copy-text', text)
+            }
         },
         created: async function () {
 
